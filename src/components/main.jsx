3f2d584fbd6adb547680b85c93eb330e500502b4
seanjas <seanjas05@gmail.com>
2024-08-29 14:46:10 +0800

    first commit

---
+++ src/components/main.jsx
@@ -0,0 +1,17 @@
+import Nav from "./nav"
+import Content from "./content"
+
+const main = () => {
+  return (
+    <div>
+        <div>
+            <Nav/>
+        </div>
+        <div>
+            <Content/>
+        </div>
+    </div>
+  )
+}
+
+export default main
